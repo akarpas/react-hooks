359820d40176705f2608013da555d0f480e7385a
Andreas Karpasitis <karpasitis@outlook.com>
2019-02-23 18:52:16 +0100

    setup basic incremenet and decrement counter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+function App() {
+  const [count, setCount] = useState(0);
 
-      </div>
-    );
+  const increment = () => {
+    setCount(count + 1);
   }
+
+  const decrement = () => {
+    if (count === 0) {
+      setCount(0);
+    } else {
+      setCount(count - 1);
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1 className="Count">{count}</h1>
+      <button className="Button" onClick={decrement}>Down</button>
+      <button className="Button" onClick={increment}>Up</button>
+    </div>
+  );
 }
 
 export default App;
